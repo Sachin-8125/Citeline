05fd93d85dd3173b7cac0618b8e21b618fbc8e0a
Sachin-8125 <devinewolf7@gmail.com>
2026-06-30 03:36:22 +0530

    Add Dashboard component and integrate it into App with protected routing; enhance user interface for research management and document handling.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard';
 import { PublicOnlyRoute } from './components/PublicOnlyRoute';
+import {ProtectedRoute} from './components/ProtectedRoute'
 
 
 function App() {
@@ -16,6 +18,9 @@ function App() {
       </Route>
       <Route element={<PublicOnlyRoute/>}>
         <Route element={<Signup/>} path="/signup" />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<Dashboard />} path="/dashboard" />
       </Route>
       <Route element={<Navigate to="/" replace />} path="*" />
     </Routes>
